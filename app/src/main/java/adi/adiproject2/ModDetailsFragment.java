bc7f891f6d916752a190cbb323440c58783342bc
pauldebellis <pvdebellis@gmail.com>
2016-09-09 09:18:50 -0700

    putting a pin in mod details

--- app/src/main/java/adi/adiproject2/ModDetailsFragment.java
+++ app/src/main/java/adi/adiproject2/ModDetailsFragment.java
@@ -1,7 +1,29 @@
 package adi.adiproject2;
 
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
 /**
- * Created by klaus_000 on 9/8/2016.
+ * A simple {@link Fragment} subclass.
  */
-public class ModDetailsFragment {
+public class ModDetailsFragment extends Fragment {
+
+
+    public ModDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mod_details, container, false);
+    }
+
 }
